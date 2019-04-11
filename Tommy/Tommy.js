fe33acf429e5f7b6e50fbd0baadd589910477e4f
Tommy V <S531823@nwmissouri.edu>
2019-04-10 20:25:22 -0500

    update

--- Tommy/Tommy.js
+++ Tommy/Tommy.js
@@ -5,8 +5,24 @@ $(document).ready(function() {
             }, 1000);
     });
 
-    $("#downAngle").mouseover(function() { //changes mouse pointer to hand when hovered over button
+    $("#downAngle, #navAboutMe, #navProjects, #navContact").mouseover(function() { //changes mouse pointer to hand when hovered over button
         $(this).css('cursor', 'pointer');
-    })
+    });
+    
+    $("#navAboutMe").click(function() { //scrolls to About Me on click. Animate smooth scroll
+        $('html, body').animate({
+            scrollTop: $("#aboutMe").offset().top
+            }, 900);
+    });
+    $("#navProjects").click(function() { //scrolls to Projects on click. Animate smooth scroll
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+            }, 900);
+    });
+    $("#navContact").click(function() { //scrolls to Contact on click. Animate smooth scroll
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+            }, 900);
+    });
 
 });
