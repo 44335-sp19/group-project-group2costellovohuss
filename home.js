c07fe038ba41c32990b439d50fae89c1bc73078a
Tommy V <S531823@nwmissouri.edu>
2019-04-11 12:56:28 -0500

    update

--- home.js
+++ home.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     $("#tommyPic").click(function () { //goes to portfolio when card in clicked
         window.location.href = "Tommy/Tommy.html";
     });
-    $("#tommyPic, #cliftonPic, #joePic").mouseover(function() { //changes mouse pointer to hand when hovered over card
-        $(this).css('cursor', 'pointer');
-    })
+    $("#tommyPic, #cliftonPic, #joePic").mouseover(function() {
+        $(this).css('cursor', 'pointer'); //changes mouse pointer to hand when hovered over card
+        $(this).css('transform', 'scale(1.1)'); //expands the card on hover
+    });
+    $("#tommyPic, #cliftonPic, #joePic").mouseout(function() {
+        $(this).css('transform', 'scale(1)'); //reverts to normal size when mouse off
+    });
 
 });
