d4cbe961997b8abb108df28e6350c3215f6a134f
Joe Costello <46724385+S528957@users.noreply.github.com>
2019-04-16 18:20:46 -0500

    page links working

---
+++ Joe/joe.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#ArtTab").click(function () {
+        window.location.href = "Art.html";
+    });
+    $("#CodeTab").click(function () {
+        window.location.href = "Code.html";
+    });
+    $("#AboutMeTab").click(function () {
+        window.location.href = "AboutMe.html";
+    });
+    });
