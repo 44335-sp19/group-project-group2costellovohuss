9c0c324550b9ebeda2432c09611f4d2e4cc0e2c6
Joe Costello <46724385+S528957@users.noreply.github.com>
2019-04-17 14:07:04 -0500

    Fixing some links

--- home.js
+++ home.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $("#tommyPic").click(function () { //goes to portfolio when card in clicked
         window.location.href = "Tommy/Tommy.html";
     });
+    $("#joePic").click(function () { 
+        window.location.href = "Joe/Joe.html";
+        });
     $("#tommyPic, #cliftonPic, #joePic").mouseover(function() {
         $(this).css('cursor', 'pointer'); //changes mouse pointer to hand when hovered over card
         $(this).css('transform', 'scale(1.1)'); //expands the card on hover
